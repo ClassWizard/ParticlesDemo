3c7dac2bc54fbd16aa54af6538571ec99465de95
ClassWizard <chenlongwei@dz.tt>
2016-03-04 15:50:30 +0800

    Initial Commit

---
+++ ParticlesDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParticlesDemo
+//
+//  Created by ClassWizard on 16/3/4.
+//  Copyright © 2016年 ClassWizard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParticlesDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParticlesDemo
+//
+//  Created by ClassWizard on 16/3/4.
+//  Copyright © 2016年 ClassWizard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
